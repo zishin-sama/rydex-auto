d1694bd42c86e6e0f87596f9ed7f2e4eb84af294
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-29 11:06:19 +0800

    Update goiadmin.js

--- script/goiadmin.js
+++ script/goiadmin.js
@@ -21,7 +21,7 @@ module.exports.handleEvent = function({ api, event, admin }) {
 					"𝖠𝗇𝗈𝗍𝗁𝖾𝗋 𝗍𝖺𝗀 𝗂𝗇 𝗆𝗒 𝖺𝖽𝗆𝗂𝗇, 𝗂 𝗐𝗂𝗅𝗅 𝗉𝗎𝗇𝖼𝗁 𝗒𝗈𝗎 🙂",
 					"busy pa ata yun kaya mag-antay ka",
 					"Sorry, naka bebetime pa don't disturb him 🙄",
-					"Do you like my creator thats why your tagging him? Why dont you add him https://www.facebook.com/swordigo.swordslush 😏",
+					"Do you like my creator thats why your tagging him? Why dont you add him https://www.facebook.com/Churchill.Dev4100 😏",
 					" Another tag in my Creator, i will kick your fucking ass"
 				];
 				api.setMessageReaction("😍", event.messageID, (err) => {}, true);
