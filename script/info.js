d8d5a7fd239168e4bd156c8e80569312ae7a4708
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-29 09:42:21 +0800

    Update info.js

--- script/info.js
+++ script/info.js
@@ -24,10 +24,10 @@ module.exports.run = async function({ api, event, args, prefix, admin }) {
 		let seconds = Math.floor(time % 60);
 		const uptimeString = `${years > 0 ? `${years} years ` : ''}${months > 0 ? `${months} months ` : ''}${weeks > 0 ? `${weeks} weeks ` : ''}${days % 7 > 0 ? `${days % 7} days ` : ''}${hours > 0 ? `${hours} hours ` : ''}${minutes > 0 ? `${minutes} minutes ` : ''}${seconds} seconds`;
 
-		const CREATORLINK = "https://www.facebook.com/swordigo.swordslush";
+		const CREATORLINK = "https://www.facebook.com/Churchill.Dev4100";
 		const BOTCREATOR = "CLIFFVINCENT";
-		const BOTNAME = "𝗬𝗔𝗭𝗞𝗬𝗕𝗢𝗧";
-		const FILESOWNER = "Cid";
+		const BOTNAME = "𝑪𝑯𝑼𝑹𝑪𝑯𝑰𝑳𝑳𝑩𝑶𝑻";
+		const FILESOWNER = "chill";
 		const juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【HH:mm:ss】");
 		const link = ["https://i.imgur.com/9LDVC57.mp4", "https://i.imgur.com/r7IxgiR.mp4", "https://i.imgur.com/J1jWubu.mp4", "https://i.imgur.com/DJylTiy.mp4", "https://i.imgur.com/v4mLGte.mp4", "https://i.imgur.com/uthREbe.mp4", "https://i.imgur.com/ee8fHna.mp4", "https://i.imgur.com/VffzOwS.mp4", "https://i.imgur.com/ci5nztg.mp4", "https://i.imgur.com/qHPeKDV.mp4", "https://i.imgur.com/Rkl5UmH.mp4",
 "https://i.imgur.com/IGXINCB.mp4",
@@ -84,9 +84,9 @@ module.exports.run = async function({ api, event, args, prefix, admin }) {
 				api.sendMessage({
 						body: `➢ Admin and Bot Information
 
-⁂ Bot Name: ${BOTNAME}
-✧ Bot Admin: 𝗖𝗹𝗶𝗳𝗳 𝗩𝗶𝗻𝗰𝗲𝗻𝘁
-♛ Bot Admin Link: https://www.facebook.com/${admin}
+⁂ Bot Name: 𝒄𝒉𝒖𝒓𝒄𝒉𝒊𝒍𝒍𝒃𝒐𝒕
+✧ Bot Admin: 𝒄𝒉𝒖𝒓𝒄𝒉𝒊𝒍𝒍 𝒂𝒃𝒊𝒏𝒈
+♛ Bot Admin Link: https://www.facebook.com/Churchill.Dev4100
 ❂ Bot Prefix: ${prefix}
 ✫ Files Owner: ${FILESOWNER}
 ➟ UPTIME ${uptimeString}
