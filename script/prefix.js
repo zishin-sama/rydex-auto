4ea1f4532c7cdc086226dfb84b4c1466ac75ff9d
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-04-29 20:11:36 +0800

    Update prefix.js

--- script/prefix.js
+++ script/prefix.js
@@ -6,37 +6,14 @@ module.exports.config = {
     role: 0,
     credits: "cliff",
     description: "Display the prefix of your bot",
-    hasPrefix: false,
     usages: "prefix",
     cooldown: 5,
     aliases: ["prefix", "Prefix", "PREFIX", "prefi"],
 };
 
 module.exports.run = function ({ api, event, prefix, admin }) {
-    const { threadID, messageID } = event;
+    const { threadID, messageID, body } = event;
 
-    if (event.body.toLowerCase() === `${prefix}prefix`) {
-        if (prefix === undefined) {
-            api.sendMessage(
-                "I don't have a prefix set.",
-                threadID,
-                messageID
-            );
-        } else {
-            api.sendMessage(
-                `My prefix is ${prefix}.`,
-                threadID,
-                messageID
-            );
-        }
-        return;
-    }
-
-    if (event.body.toLowerCase().startsWith(prefix)) {
-        // Rest of the code for handling commands
-    }
-
-    // If prefix is not set
     if (!prefix) {
         api.sendMessage(
             "I don't have a prefix set.",
@@ -46,10 +23,37 @@ module.exports.run = function ({ api, event, prefix, admin }) {
         return;
     }
 
-    // If a prefix is set
+    // Check if the command is invoked manually with the prefix
+    if (body.toLowerCase() === `${prefix}prefix`) {
+        api.sendMessage(
+            `Hey there! My prefix is [ 𓆩 ${prefix} 𓆪 ].`,
+            threadID,
+            messageID
+        );
+        return;
+    }
+
+    // Sending the message along with the attachment
     api.sendMessage(
-        `My prefix is "${prefix}".`,
+        {
+            body: `Yo, my prefix is [ 𓆩 ${prefix} 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/Churchill.Dev4100`,
+            attachment: fs.createReadStream(__dirname + "/cache2/prefix.jpeg")
+        },
         threadID,
-        messageID
+        (err, messageInfo) => {
+            if (err) return console.error(err);
+
+            const voiceFile = fs.readFileSync(__dirname + "/cache2/prefix.jpeg");
+            api.sendMessage(
+                {
+                    attachment: voiceFile,
+                    type: "audio",
+                    body: "Hey, listen to my prefix information!",
+                },
+                threadID,
+                () => {}
+            );
+            api.setMessageReaction("🚀", messageInfo.messageID, (err) => {}, true);
+        }
     );
 };
