9420ec770e28a689af9ffb4e397276d97ac2059f
Churchillabing1 <141407524+Churchillabing1@users.noreply.github.com>
2024-05-04 18:24:11 +0800

    Update women.js

--- script/women.js
+++ script/women.js
@@ -1,11 +1,8 @@
-const fs = require("fs");
-const request = require("request");
-
 module.exports.config = {
     name: "women",
     version: "1.0.0",
     role: 0,
-    credits: "churchill",
+    credits: "ChatGPT",
     description: "Send a video of women",
     hasPrefix: true,
     usage: "",
@@ -14,20 +11,13 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event }) {
     const { threadID, messageID } = event;
-    const link = [
-        "https://i.imgur.com/z47Hvel.mp4",
-        // Dito mo ilalagay ang mga iba pang mga links ng video
-    ];
+    const videoLink = "https://i.imgur.com/z47Hvel.mp4";
 
-    // Pumili ng isang random na link mula sa listahan
-    const randomIndex = Math.floor(Math.random() * link.length);
-    const videoLink = link[randomIndex];
-
-    // Padala ng video
+    // Send video
     await api.sendMessage({
         body: "women ☕",
         attachment: request(videoLink)
     }, threadID, () => {
-        api.setMessageReaction("☕", messageID);
+        api.setMessageReaction("❤️", messageID);
     });
 };
