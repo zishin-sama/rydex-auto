dcf7b8c8d894fd1bdb1f546e69d9c1a69b7b5f11
Churchill <141407524+Churchillabing1@users.noreply.github.com>
2024-05-07 21:02:32 +0800

    Update and rename ai2.js to ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,15 +1,17 @@
 const axios = require('axios');
+
 module.exports.config = {
-  name: 'ai2',
+  name: 'ai',
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
   aliases: ['gpt', 'openai'],
   description: "An AI command powered by GPT-4",
-  usage: "ai2 [promot]",
-  credits: 'Developer',
+  usage: "ai [prompt]",
+  credits: 'Developer: https://www.facebook.com/Churchill.Dev4100',
   cooldown: 3,
 };
+
 module.exports.run = async function({
   api,
   event,
@@ -27,11 +29,10 @@ module.exports.run = async function({
   api.sendMessage(`🔍𝙎𝙚𝙖𝙧𝙘𝙝𝙞𝙣𝙜 𝙋𝙡𝙚𝙖𝙨𝙚 𝙒𝙖𝙞𝙩....
 ━━━━━━━━━━━━━━━━━━\n\n "${input}"`, event.threadID, event.messageID);
   try {
-    const {
-      data
-    } = await axios.get(`https://openaikey-x20f.onrender.com/api?prompt=${encodeURIComponent(input)}`);
+    const { data } = await axios.get(`https://openaikey-x20f.onrender.com/api?prompt=${encodeURIComponent(input)}`);
     const response = data.response;
-    api.sendMessage(response, event.threadID, event.messageID);
+    const credits = '\n𝐜𝐫𝐞𝐝𝐢𝐭𝐬: https://www.facebook.com/Churchill.Dev4100';
+    api.sendMessage(response + credits, event.threadID, event.messageID);
   } catch (error) {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
