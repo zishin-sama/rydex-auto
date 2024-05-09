a01f95460211eec2c3f455fb1d0ac53e0475998b
Churchill <141407524+Churchillabing1@users.noreply.github.com>
2024-05-09 20:25:03 +0800

    Update sendnoti.js

--- script/sendnoti.js
+++ script/sendnoti.js
@@ -21,7 +21,7 @@ module.exports.run = async function ({ api, event, args, admin }) {
 	async function sendMessage(thread) {
 		try {
 			await api.sendMessage(
-`🟢🟡🔴\n ----------------\n『 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍 』\n\n ----------------\n𝑴𝒆𝒔𝒔𝒂𝒈𝒆 𝒇𝒓𝒐𝒎 𝒂𝒅𝒎𝒊𝒏:「${custom}」\n 𝐀𝐝𝐦𝐢𝐧 𝐧𝐚𝐦𝐞: 𝐂𝐡𝐮𝐫𝐜𝐡𝐢𝐥𝐥\n _________________________`,
+`🟢🟡🔴\n ----------------\n『 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍 』\n\n ----------------\n𝑴𝒆𝒔𝒔𝒂𝒈𝒆 𝒇𝒓𝒐𝒎 𝒂𝒅𝒎𝒊𝒏:「${custom}」\n 𝐟𝐨𝐥𝐥𝐨𝐰 𝐝𝐞𝐯:https://www.facebook.com/Churchill.Dev4100\n _________________________`,
 				thread.threadID
 			);
 			sentCount++;
