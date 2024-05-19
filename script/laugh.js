453b5581e885b2cfd91a914c0b90293896cf896b
Churchill <141407524+Churchillabing1@users.noreply.github.com>
2024-05-19 21:28:25 +0800

    Update laugh.js

--- script/laugh.js
+++ script/laugh.js
@@ -2,16 +2,16 @@ module.exports.config = {
 	name: 'laugh',
 	version: '1.0.0',
 	hasPermision: 0,
-	credits: 'tite',
+	credits: 'Ychilli.',
 	usePrefix: false,
-	description: 'mag automatic mag send pag may tumawa',
+	description: 'Responds to laughter with a predefined message',
 	commandCategory: 'fun',
 	usages: '',
 	cooldowns: 0
 };
 
 module.exports.run = async ({ api, event }) => {
-	const laughterPatterns = /(ha|he|hi|ho|hu|lol|rofl|lmao|😂|🤣)/i;
+	const laughterPatterns = /(ha|he|hi|ho|hu|lol|rofl|lmao|😂|🤣|kek|lel)/i;
 	const message = `HAPPY? CHAT MO CREATOR KO ETO fb link: https://www.facebook.com/Churchill.Dev4100`;
 
 	if (laughterPatterns.test(event.body)) {
