bd5c0d18378b14780eebb4b3dd974d5d3535f0e8
Churchill <141407524+Churchillabing1@users.noreply.github.com>
2024-05-19 21:25:02 +0800

    Update women.js

--- script/women.js
+++ script/women.js
@@ -6,8 +6,8 @@ module.exports.config = {
 	name: 'women',
 	version: '1.0.0',
 	hasPermision: 0,
-	credits: 'chillimansi',
-	usePrefix: false,
+	credits: 'chilli',
+	usePrefix: true,
 	description: 'Send a video from Google Drive',
 	commandCategory: 'media',
 	usages: '',
@@ -15,30 +15,28 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ api, event }) => {
+	const videoUrl = "https://drive.google.com/uc?export=download&id=1--lgvkF6kOckzZe27gWeQ54ITjTRrqrk";
+	const tmpFolderPath = path.join(__dirname, 'tmp');
+
+	if (!fs.existsSync(tmpFolderPath)) {
+		fs.mkdirSync(tmpFolderPath);
+	}
+
+	const filePath = path.join(tmpFolderPath, 'women_video.mp4');
+
 	try {
-		const videoUrl = "https://drive.google.com/uc?export=download&id=1--lgvkF6kOckzZe27gWeQ54ITjTRrqrk";
-		const tmpFolderPath = path.join(__dirname, 'tmp');
+		const response = await axios.get(videoUrl, { responseType: 'arraybuffer' });
+		fs.writeFileSync(filePath, Buffer.from(response.data, 'binary'));
 
-		if (!fs.existsSync(tmpFolderPath)) {
-			fs.mkdirSync(tmpFolderPath);
-		}
-
-		const filePath = path.join(tmpFolderPath, 'women_video.mp4');
-
-		// Download the video
-		const videoResponse = await axios.get(videoUrl, { responseType: 'arraybuffer' });
-		fs.writeFileSync(filePath, Buffer.from(videoResponse.data, 'binary'));
-
-		// Send the video
-		await api.sendMessage({
+		const message = {
+			body: "WOMEN KAPE!",
 			attachment: fs.createReadStream(filePath)
-		}, event.threadID, event.messageID);
+		};
 
-		// Delete the video after sending the message
-		fs.unlinkSync(filePath);
-
+		await api.sendMessage(message, event.threadID, event.messageID);
+		fs.unlinkSync(filePath); // Delete the video after sending
 	} catch (error) {
 		console.error('Error in women command:', error);
-		return api.sendMessage('An error occurred while processing the command.', event.threadID);
+		api.sendMessage('An error occurred while processing the command.', event.threadID, event.messageID);
 	}
 };
