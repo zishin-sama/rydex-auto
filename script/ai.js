9d42fb99f1704d1ffc261dd7b795779e1cf913fe
NexusLevel999 <nexuskaizu@gmail.com>
2024-05-31 17:36:31 +0800

    .

---
+++ script/ai.js
@@ -0,0 +1,20 @@
+module.exports = {
+  config: {
+    name: "ai",
+    description: "Talk to GPT (conversational)",
+    hasPrefix: false,
+    usage: "[ask]",
+    role: 0
+  },
+  start: async function ({ text, reply, react, event}) {
+    let p = text.join(' '), uid = event.senderID;
+    const axios = require('axios');
+    if (!p) return reply('Please enter a prompt.');
+    try {
+      const r = (await axios.get(`https://deku-rest-api-3ijr.onrender.com/gpt4?prompt=${p}&uid=${uid}`)).data;
+      return reply(r.gpt4);
+    } catch (g) {
+      return reply(g.message);
+    }
+  }
+}
