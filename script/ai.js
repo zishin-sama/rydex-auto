125695eee11afa8cfb6e6bc448f52ecf91b60d4c
NexusLevel999 <nexuskaizu@gmail.com>
2024-06-19 05:07:19 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -18,7 +18,7 @@ module.exports.run = async function({ api, event, args }) {
   }
 
   try {
-    const response = await axios.get(`https://deku-rest-api-3ijr.onrender.com/gpt4?prompt=${prompt}&uid=${uid}`);
+    const response = await axios.get(`https://deku-rest-api-pywad.onrender.com/gpt4?prompt=${prompt}&uid=${uid}`);
     return api.sendMessage(response.data.gpt4, event.threadID);
   } catch (error) {
     return api.sendMessage(error.message, event.threadID);
