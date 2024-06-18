733622ccd8228065ac5c4de3582f9bdd013610d4
NexusLevel999 <nexuskaizu@gmail.com>
2024-06-19 06:51:03 +0800

    .

--- script/time.js
+++ script/time.js
@@ -1,13 +1,15 @@
 module.exports.config = {
   name: "time",
   description: "Get the current time in Manila, Philippines.",
-  aliases: ["what time is it?"],
+  aliases: [],
+  role: 0,
   credits: "aze kagenou",
+  hasPrefix: false,
   cooldown: 5
 };  
 module.exports.run = async function({api, event}) {
     const now = new Date();
     const options = { timeZone: 'Asia/Manila', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
     const formattedTime = now.toLocaleString('en-US', options);
-    api.sendMessage(formattedTime, event.threadID, event.messageID);
+    api.sendMessage(`The current time is: ${formattedTime}`, event.threadID, event.messageID);
     };
