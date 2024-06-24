8dabdd735c43fad0a79edfb5347e759196775c7b
NexusLevel999 <nexuskaizu@gmail.com>
2024-06-25 05:04:31 +0800

    Update simreply.js

--- script/event/simreply.js
+++ script/event/simreply.js
@@ -1,19 +1,17 @@
 const axios = require("axios");
+
 module.exports.config = {
-    name: "sim",
-    version: "1.0.0"
+  name: "sim",
+  version: "1.0.0"
 };
 module.exports.handleEvent = async function({ api, event }) {
-	const query = event.body;
+  if (event.type === "message_reply" && event.messageReply && event.messageReply.senderID === api.getCurrentUserID()) {
+    const q = encodeURIComponent(event.body);
     try {
-        if (event.type === "message_reply") {
-        if (event.senderID === event.messageReply.senderID) {
-        	return;
-        }
-     }
-        const response = await axios.get(`https://sim-api-ctqz.onrender.com/sim?query=${encodeURIComponent(query)}`);
-            api.sendMessage(response.data, event.threadID, event.messageID);
+      const { data } = await axios.get(`https://sim-api-ctqz.onrender.com/sim?query=${q}`);
+      api.sendMessage(data.respond, event.threadID, event.messageID);
     } catch (error) {
-        console.error("An error occurred:", error);
+      console.error("Error fetching response:", error);
     }
+  }
 };
