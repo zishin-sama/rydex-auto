99643e11d0c1abe043eeaaa05f95a8b215f6de51
NexusLevel999 <nexuskaizu@gmail.com>
2024-06-27 18:35:09 +0800

    Update changebio.js

--- script/changebio.js
+++ script/changebio.js
@@ -7,18 +7,18 @@ module.exports.config = {
   hasPrefix: true,
   usage: "changebio [text]",
   aliases: ["cb"],
-  cooldown: 10,
+  cooldown: 5,
 };
 module.exports.run = async function ({ api, event, args }) {
     const newBio = args.join(" ");
-    if (!newBio) {
-      return api.sendMessage("Invalid Argument!", event.threadID, event.messageID);
-    } else {
+    if (newBio) {
       try {
         await api.changeBio(newBio, true);
         api.sendMessage("Set bot biography to: " + newBio, event.threadID, event.messageID);
       } catch (error) {
         api.sendMessage("Failed to change bio. Please try again later.", event.threadID, event.messageID);
     }
-  }
+  } else {
+      api.changeBio(newBio, true);
+    }
 };
