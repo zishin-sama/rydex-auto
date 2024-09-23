3e39e49d3473f87501928db6ecb7f7b68bdacafc
rydextrex-ng <rydextrex@gmail.com>
2024-09-23 19:27:23 +0800

    Update and rename blackbox.js to 0ai2.js

--- script/0ai2.js
+++ script/0ai2.js
@@ -4,10 +4,10 @@ module.exports.config = {
 	version: "1.0.0",
 	role: 0,
 	description: "Chat with BlackboxAI (Conversational)",
-	aliases: ["bb"],
+	aliases: ["bbox"],
 	hasPrefix: true,
 	usage: "{n}prompt",
-	credits: "rydex|api by josh",
+	credits: "Rydex",
 	cooldown: 5
 }
 module.exports.run = async ({api, args, event}) => {
@@ -20,13 +20,19 @@ module.exports.run = async ({api, args, event}) => {
 	if (!prompt) return reply("Please provide a prompt");
 	
 	try {
+		await api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
+    
+    api.sendTypingIndicator(event.threadID, true); 
+    
 		const url = `https:\/\/deku-rest-api.gleeze.com/api/blackboxai?q=${prompt}&uid=${uid}`;
 		const res = await ax.get(url);
 		const d = res.data.result;
+		await api.setMessageReaction("✅", event.messageID, (err) => {}, true);
 		return reply(d);
 		
 	}
 	catch (e) {
+		await api.setMessageReaction("❌", event.messageID, (err) => {}, true);
 		return reply(e.message);
 	}
 }
