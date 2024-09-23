711fcf1317c459720862e6ece84cb2f5e59c2536
rydextrex-ng <rydextrex@gmail.com>
2024-09-23 19:33:33 +0800

    Update and rename ai.js to 0ai.js

---
+++ script/0ai.js
@@ -0,0 +1,45 @@
+module.exports.config = {
+		name: "gpt",
+		version: "1.0.0",
+		role: 0,
+		credits: "Rydex",
+		hasPrefix: true,
+		description: "AI-powered responses GPT-4.",
+		usage: "[prompt]",
+		cooldown: 0,
+	    aliases: ["ai"]
+};
+
+module.exports.run = async function ({ api, event, args }) {
+		try {
+				const { G4F } = require("g4f");
+
+				function reply(a) {
+						api.sendMessage(a, event.threadID, event.messageID);
+				}
+
+				const g4f = new G4F();
+				
+				const input = args.join(' ');
+				if (!input) return reply('Please provide a prompt.');
+				
+				await api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
+    
+    api.sendTypingIndicator(event.threadID, true); 
+
+				const messages = 
+                { role: "user", content: input }
+				const options = {
+						provider: g4f.providers.GPT,
+						model: "gpt-4",
+						debug: true,
+						proxy: ""
+				};
+				const response = await g4f.chatCompletion(messages, options);
+				await api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+				reply(response);
+		} catch (e) {
+			await api.setMessageReaction("❌", event.messageID, (err) => {}, true);
+				return reply(e.message);
+		}
+}
